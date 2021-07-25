e06ca35682585abed6facbdf2db71363d41b5b93
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2021-07-24 23:41:21 -0300

    Correction for ServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,19 @@
 
 namespace App\Providers;
 
-use App\Domain\Interfaces\INotificador;
 use App\Domain\Interfaces\Services\IItemService;
 use App\Domain\Interfaces\Services\IUsuarioService;
-use App\Domain\Notificador;
+
+use App\Domain\Services\ItemService;
 use App\Domain\Services\UsuarioService;
+
 use App\Repository\ItemRepository;
 use App\Repository\UsuarioRepository;
-use Illuminate\Support\ServiceProvider;
+
 use App\Domain\Interfaces\Repository\IItemRepository;
 use App\Domain\Interfaces\Repository\IUsuarioRepository;
+
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(INotificador::class, Notificador::class);
-
         $this->app->bind(IUsuarioService::class, UsuarioService::class);
+        $this->app->bind(IItemService::class, ItemService::class);
+
 
         $this->app->bind(IItemRepository::class, ItemRepository::class);
         $this->app->bind(IUsuarioRepository::class, UsuarioRepository::class);
